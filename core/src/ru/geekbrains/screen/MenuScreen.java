d9a60673da83f2fbac30992341f8ab61b5693e6e
StanislavYak <karueli59@gmail.com>
2018-10-30 15:42:39 +0300

    dz 3

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -9,26 +9,24 @@ import com.badlogic.gdx.math.Vector2;
 import ru.geekbrains.base.Base2DScreen;
 
 public class MenuScreen extends Base2DScreen {
-    private SpriteBatch batch;
+
     private Texture img;
 
     private Vector2 pos;
     private Vector2 v;
-    private Vector2 posEnd;
-    private  int y;
-
+    private Vector2 touch;
+    private Vector2 buf;
 
 
     @Override
     public void show(){
         super.show();
-        Gdx.input.setInputProcessor(this);
-        y = Gdx.graphics.getHeight();
         batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
         pos = new Vector2(0,0);
-        v = new Vector2(0, 0);
-        posEnd = new Vector2(0,0);
+        v = new Vector2();
+        touch = new Vector2();
+        buf = new Vector2();
 
     }
 
@@ -37,31 +35,30 @@ public class MenuScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClearColor(0.128f, 0.50f, 0.9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        buf.set(touch);
+        if (buf.sub(pos).len() > v.len()) {
+            pos.add(v);
+        }else{
+            pos.set(touch);
+        }
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        batch.draw(img, pos.x, pos.y, 42f, 42f);
         batch.end();
-        newPos();
+
     }
 
-    private void newPos() {
-        if (pos.dst(posEnd) > 1) {
-            pos.add(v);
-        }
-    }
 
     @Override
     public  void dispose(){
-        batch.dispose();
         img.dispose();
         super.dispose();
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        posEnd.x = screenX;
-        posEnd.y = y - screenY;
-        v = posEnd.cpy().sub(pos).nor();
+        this.touch = touch;
+        v.set(touch.cpy().sub(pos). scl(0.01f));
 
-        return super.touchDown(screenX, screenY, pointer, button);
+        return false;
     }
 }
